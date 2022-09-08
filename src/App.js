c443dfca5499b3c2ca374ecb055fe88d85354cda
Sujay Naik <112873531+SujayNaikk@users.noreply.github.com>
2022-09-08 11:12:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return(
+    <div className="App">
+      <h1>WELCOME TO ASSIGNMENT-ONE</h1>
+       <h3> DONE AND DUSTED</h3>
+       <h6> THANK-YOU</h6>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+
+import React,{component} from 'react';
+
+export class Home extends component{
+
+    render(){
+        <div className="mt-5 d-flex justify-content-left">
+            <h3>WELCOME TO JUMANJI.
+                This is the Home
+            </h3>
+        </div>
+    }
+}
